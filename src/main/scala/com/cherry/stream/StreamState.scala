aa2d59f35b3349894315a12f503e86a340b346b6
venkata-charan <charanraj.lv@gmail.com>
2019-02-21 12:47:52 +0530

    Added kafka simple consumer

--- src/main/scala/com/cherry/stream/StreamState.scala
+++ src/main/scala/com/cherry/stream/StreamState.scala
@@ -2,7 +2,7 @@ package com.cherry.stream
 
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.kafka.KafkaUtils
-import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import kafka.serializer.StringDecoder
 
 
@@ -20,45 +20,15 @@ object StreamState extends App {
   val messages  =
     KafkaUtils
       .createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicset)
-//      .map(x => (x._1,1))
-//      .reduceByKey(_+_)
-//      .repartition(1).saveAsTextFiles("hdfs:///user/charanrajlv3971/kafkaDirectStream")
 
   // Get the lines, split them into words, count the words and print
   val lines = messages.map(_._2)
-  val words = lines.flatMap(_.split(" "))
+  val words = lines.flatMap(_.split(","))
   val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _)
   wordCounts.print()
-
-
-//  lines.foreachRDD( rdd => {
-//
-//    if( rdd.count()>0 ) {
-//      println("printing messages from kafka")
-//      val repart_Rdd = rdd.repartition(1).cache()
-//      repart_Rdd.collect()
-//    }
-//
-//  })
-
-//    .map(x => {
-//      val list1 = x._2.split(",")
-//      val ip = list1(2)
-//      (ip, 1)
-//    })
-//
-//  val runningCounts = lines.updateStateByKey[Int](updateFunction _)
-//  runningCounts.print()
 
   ssc.checkpoint("hdfs:///user/charanrajlv3971/checkpoint/")
   ssc.start()
   ssc.awaitTermination()
-
-  def updateFunction(newValues: Seq[Int], oldStateCount: Option[Int]): Option[Int] = {
-    val newCount = newValues.sum
-    val previousCount = oldStateCount.getOrElse(0)
-    Some(newCount + previousCount)
-  }
-
 
 }
